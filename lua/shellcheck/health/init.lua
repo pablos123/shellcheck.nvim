757aaab049c317863a1d975e590b2de271964b33
Pablo <pablosaavedra123@gmail.com>
2024-10-29 23:13:17 -0300

    another typo

--- lua/shellcheck/health/init.lua
+++ lua/shellcheck/health/init.lua
@@ -5,13 +5,13 @@ M.check = function()
     if vim.fn.executable('shellcheck') == 1 then
         vim.health.ok('shellcheck found in $PATH')
     else
-        vim.health.erro('shellcheck not found in $PATH')
+        vim.health.error('shellcheck not found in $PATH')
     end
 
     if vim.fn.has('nvim-0.7') == 1 then
         vim.health.ok('nvim >= 0.7')
     else
-        vim.health.erro('nvim version is below 0.7')
+        vim.health.error('nvim version is below 0.7')
     end
 end
 
